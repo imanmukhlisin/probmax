842fdbdecc5b534f952137faf6d7472a98ac3287
Thirza Rep <thirza-rep@users.noreply.github.com>
2026-01-31 13:20:17 +0700

    Fix CORS allowing credentials

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://probmaxcare-frontend.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +33,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
