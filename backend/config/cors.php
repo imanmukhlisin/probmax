b447da0c1fce3b03e7a0f9c4a76e2b7f14898a85
Thirza Rep <thirza-rep@users.noreply.github.com>
2026-01-31 16:57:40 +0700

    Fix CORS trailing slash

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://www.probmaxcare.site/'
+        'https://www.probmaxcare.site'
     ],
 
     'allowed_origins_patterns' => [],
