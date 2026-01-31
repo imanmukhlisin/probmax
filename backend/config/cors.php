331dbba625fade9d68a628dfc8ba7856f37e008c
Thirza Rep <thirza-rep@users.noreply.github.com>
2026-01-31 16:48:52 +0700

    Add debug probe

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://probmaxcare-frontend.vercel.app'
+        'https://www.probmaxcare.site/'
     ],
 
     'allowed_origins_patterns' => [],
