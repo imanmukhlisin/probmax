b44dd81ce41faf3556e167e4a4d9cfe85073c6d3
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-01-31 20:42:06 +0700

    Fix: Remove duplicate CORS headers

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-// Brute-force CORS fix to bypass middleware issues
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
