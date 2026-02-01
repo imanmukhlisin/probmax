4c993b2e68fd38b82396c9023843c3ae68277e29
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-02-01 23:08:26 +0700

    deploy: move api folder to root for native vercel support

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Forward request to Laravel's public entry point inside backend folder
+require __DIR__ . '/backend/public/index.php';
--- backend/api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Forward request to Laravel's public entry point
-require __DIR__ . '/../public/index.php';
