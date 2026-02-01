1466549a3392ef474af50a3d57ea92af442cf520
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-02-01 23:56:28 +0700

    deploy: finalize serverless structure

--- api/index.php
+++ api/index.php
@@ -1,16 +1,4 @@
 <?php
 
-// Normalisasi path agar Vercel mendeteksi autoload dari root
-require __DIR__ . '/../vendor/autoload.php';
-
-// Bootstrap Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Run Kernel
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Langsung panggil index utama Laravel
+require __DIR__ . '/../public/index.php';
