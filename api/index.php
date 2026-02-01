942ba2ad7bbf55f8da011dcb5897ef71fee204be
imanmukhlisin <imanmukhlisin1@gmail.com>
2026-02-02 00:20:44 +0700

    deploy: fix vercel path detection by overriding script name

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
+// Fix Vercel Path Detection
+// Laravel kadang menganggap app berjalan di subfolder '/api' karena file ini ada di sana.
+// Kita paksa agar dia berpikir berjalan di root '/index.php'.
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
 // Langsung panggil index utama Laravel
 require __DIR__ . '/../public/index.php';
